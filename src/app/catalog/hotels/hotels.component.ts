6c5afe72efe1158b30eceec2a0b942bd56c336bf
Valeri <csvalkata@gmail.com>
2021-08-15 14:26:23 +0300

    finishing touches

--- src/app/catalog/hotels/hotels.component.ts
+++ src/app/catalog/hotels/hotels.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { delay } from 'rxjs/operators';
 import { ContentService } from 'src/app/services/content.service';
@@ -12,15 +13,26 @@ import { IHotel } from 'src/app/shared/interfaces';
 export class HotelsComponent implements OnInit {
 
   hotels$!: Observable<IHotel[]>;
+  query: string | undefined;
+  inSearch: boolean = false;
+  searchedDest: string = '';
 
-  constructor(private contentService: ContentService) { }
+  constructor(
+    private contentService: ContentService,
+    private activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
-    this.fetchHotels();
-  }
 
-  fetchHotels() {
-    this.hotels$ = this.contentService.fetchHotels().pipe(
+    let { dest } = this.activatedRoute.snapshot.queryParams;
+
+    if(dest) {
+      this.inSearch = true;
+      this.searchedDest = dest;
+      dest = dest.toLocaleLowerCase();
+    }
+
+    this.hotels$ = this.contentService.fetchHotels(dest).pipe(
       delay(500)
     );
   }
